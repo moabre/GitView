1e74a871986c7e4720a526e38256cc6383fc4cc7
moabre <muhammadrehman21@gmail.com>
2021-01-17 20:18:51 -0600

    created function to make donut and pie chart for the languages used

--- git-view/src/Components/UserData/UserComponents/Charts.js
+++ git-view/src/Components/UserData/UserComponents/Charts.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import ConstructCharts from '../UserComponents/ConstructChart'
 import { languageColors, backgroundColor, borderColor } from '../../Utils'
+import { RepoIcon } from '@primer/octicons-react'
 
 function Charts({ langData, repoData }) {
   console.log(langData)
@@ -32,7 +33,7 @@ function Charts({ langData, repoData }) {
 
     if (data.length > 0) {
       const chartType = 'bar'
-      const scales = true
+      const scale = true
       const legend = false
       const config = {
         ctx,
@@ -41,13 +42,86 @@ function Charts({ langData, repoData }) {
         data,
         backgroundColor,
         borderColor,
-        scales,
+        scale,
         legend,
       }
+      ConstructCharts(config)
     }
   }
+  // creating the code block for most used languages
+
+  // creating state to check for errors
+  const [chartLangData, setChartLangData] = useState(null)
+
+  //function to render the language piechart
+  const createLangChart = () => {
+    const ctx = document.getElementById('langChart')
+    const labels = langData.map((lang) => lang.label)
+    const data = langData.map((lang) => lang.value)
+
+    setChartLangData(data)
+
+    if (data.length > 0) {
+      const chartType = 'pie'
+      const scale = false
+      const legend = true
+      // converting color from the langData to alpha hex values
+      const backgroundColor = langData.map(
+        ({ color }) =>
+          `#${color.length > 4 ? color.slice(1) : color.slice(1).repeat(2)}B3`
+      )
+      const borderColor = langData.map((lang) => `${lang.color}`)
+      console.log(backgroundColor)
+      const config = {
+        ctx,
+        chartType,
+        data,
+        labels,
+        scale,
+        legend,
+        borderColor,
+        backgroundColor,
+      }
+      ConstructCharts(config)
+    }
+  }
+  // creating the code block for most stars per language
+
+  // creating state to check for errors
+  const [starLangData, setStarLangData] = useState(null)
+
+  //function to render the most stared language piechart
+  const starLang = () => {
+    const cxt = document.getElementById('starLangChart')
+    // getting own repos and starred
+    const filteredRepo = repoData.filter(
+      (repo) => !repo.fork && repo.stargazers_count > 0
+    )
+    // getting all the unique languages from our filtered data
+    const nullLang = filteredRepo.filter((repo) => repo.language === 'HTML')
+    const uniqueLang = new Set(filteredRepo.map((repo) => repo.language))
+    //creating the labels for the donut chart
+    const labels = [...uniqueLang].filter((l) => l)
+    //creating the data points for the most stars per language
+    const data = labels.map((l) => {
+      // filter repo by language data
+      const repos = filteredRepo.filter((repo) => repo.language === l)
+      // place star count in a new array for language
+      const starData = repos.map((r) => r.stargazers_count)
+      // sum total value of stars of the specific language
+      const starSum = starData.reduce((a, b) => a + b, 0)
+      //return value
+      return starSum
+    })
+
+    console.log(labels)
+    console.log(data)
+  }
+
   useEffect(() => {
-    starChart()
+    // starChart()
+    // createLangChart()
+    starLang()
   }, [])
 
   return <div></div>
